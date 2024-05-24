842fb47a01d2aa2ae57cb6bc1e64c755430539c7
IvanoskiHarmonia <macohiho@gmail.com>
2024-05-24 12:25:40 -0500

    GoBackButton is finally out of the picture (no need since I have home, and going back is not needed since not JobCard links move you to new tab. Also improved the functionality of NavBar, and looks of JobCard.

--- client/src/ui/modules/components/card/Navbar.js
+++ client/src/ui/modules/components/card/Navbar.js
@@ -1,27 +1,38 @@
 import React from "react";
-import GoBackButton from "../utils/GoBackButton";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../../../common/hooks/useAuth";
+import { LogOut } from "react-feather";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 const Navbar = () => {
-	const { isAuthenticated, userId } = useAuth();
+	const { isAuthenticated, userId, handleLogout } = useAuth();
 
 	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<div className="container">
-				{isAuthenticated && (
-					<div className="d-flex justify-content-between align-items-center w-100">
-						<div className="d-flex">
-							<GoBackButton />
-							<Link to="/" className="ms-3">
-								Home
-							</Link>
+		<>
+			{isAuthenticated && (
+				<nav className="navbar navbar-expand-lg navbar-light bg-light">
+					<div className="container">
+						<div className="d-flex justify-content-between align-items-center w-100">
+							<div className="d-flex">
+								<Link to="/" className="ms-3">
+									Home
+								</Link>
+							</div>
+							<div className="d-flex align-items-center">
+								<Link to={`/user-applications/${userId}`} target="_blank" rel="noopener noreferrer" className="me-3">
+									Applications
+								</Link>
+								<OverlayTrigger placement="bottom" overlay={<Tooltip id="logout-tooltip">Click to logout</Tooltip>}>
+									<button className="btn btn-link text-decoration-none" onClick={handleLogout} aria-label="Logout">
+										<LogOut />
+									</button>
+								</OverlayTrigger>
+							</div>
 						</div>
-						<Link to={`/user-applications/${userId}`}>Applications</Link>
 					</div>
-				)}
-			</div>
-		</nav>
+				</nav>
+			)}
+		</>
 	);
 };
 
