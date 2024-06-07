2ec33c0741ad2a15bc467508180c6b4e42c4255c
IvanoskiHarmonia <macohiho@gmail.com>
2024-06-07 11:48:02 -0500

    JobApplication now also checks if user has applied for the job. Applications has a spinner now since fetching all the places the user has applied takes time, so placeholder was needed.

--- client/src/ui/pages/job_application/JobApplication.js
+++ client/src/ui/pages/job_application/JobApplication.js
@@ -3,10 +3,50 @@ import JobFields from "./components/JobFields";
 import JobDetails from "../../modules/components/card/JobDetails/JobDetails";
 import OffCanvas from "./small_blocks/OffCanvas";
 import StyleWrapper from "../../modules/components/wrappers/StyleWrapper";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useAuth } from "../../../common/hooks/useAuth";
+import axios from "axios";
+import Spinner from "react-bootstrap/Spinner";
 
 function JobApplication() {
 	const [job, setJob] = useState({});
+	const { jobId } = useParams();
+	const { userId } = useAuth();
+	const navigate = useNavigate();
+	const [loading, setLoading] = useState(true);
+	const [hasApplied, setHasApplied] = useState(false);
+
+	useEffect(() => {
+		const checlIfUserApplied = async () => {
+			try {
+				const response = await axios.get("http://localhost:8000/api/user-applications/check-application/" + userId + "/" + jobId);
+				if (response.status === 200) {
+					setHasApplied(true);
+				} else {
+					console.error("Failed to check if user applied:", response.data);
+				}
+			} catch (error) {
+				console.error("Failed to check if user applied:", error.response ? error.response.data : error.message);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		checlIfUserApplied();
+	}, [userId, jobId]);
+
+	if (loading) {
+		return (
+			<div className="overlay">
+				<Spinner animation="grow" variant="primary" />
+			</div>
+		);
+	}
+
+	if (hasApplied) {
+		navigate("/careers/" + jobId);
+	}
 
 	return (
 		<StyleWrapper>
