e50eac7c516c33292f456f665824ccc898d41803
IvanoskiHarmonia <macohiho@gmail.com>
2024-06-10 13:46:00 -0500

    Fixed the footer not sticking to the bottom of the page.

--- client/src/ui/modules/components/card/Footer/Footer.js
+++ client/src/ui/modules/components/card/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 export default function Footer() {
 	return (
-		<footer className="footer navbar-fixed-bottom">
+		<footer className="footer mt-auto navbar-fixed-bottom">
 			<div className="container d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
 				<div className="col-md-4 d-flex align-items-center">
 					<span className="mb-3 mb-md-0 text-body-secondary">© 2024 OpenCareerPortal</span>
